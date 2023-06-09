8788bdd84d90431723f44f1861cccda084593502
Ayan <ajan.kohan@gmail.com>
2023-06-10 02:06:21 +0600

    maps

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> result = new HashMap<>();
+        for(int i=1;i<=sourceMap.size();i++){
+            System.out.println(i+" "+sourceMap.get(i));
+            if (sourceMap.get(i) != null){
+                result.put(sourceMap.get(i),i);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        sentence = sentence.replaceAll("\\pP","").toLowerCase();
+        Map<String, Integer> result = new HashMap<>();
+        for(String s : sentence.split(" ")){
+            Integer count = result.get(s);
+            if(!s.isEmpty()){
+                result.put(s, count==null ? 1 : count+1);
+            }
+        }
+        return result;
     }
 }
